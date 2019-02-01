65436712d7baf4ed9319bd21fbb8d1db28f8a139
Hibald-Tybor <rehounek.miroslav@gmail.com>
2019-02-01 20:01:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-contact.component";
+import EditTodo from "./components/edit-contact.component";
+import TodosList from "./components/contacts-list.component";
+import Quiz from "./components/quiz.component";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+              <ul className="nav justify-content-center nav-tabs">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">Contacts</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/quiz" className="nav-link">Quiz</Link>
+                </li>
+              </ul>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+          <Route path="/quiz" component={Quiz} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/quiz.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Quiz extends React.Component {
+    render() {
+        return(
+            <div class="container">
+                <p>Quiz</p>
+            </div>
+        )
+    }
+}
+
+export default Quiz
